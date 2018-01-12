e704afa7661a39e21e841be6e2ea273f417d17e1
jmario mejia <jmariomejiap@gmail.com>
2018-01-11 21:40:00 -0800

    removing redux App sample folder and files

--- client/reducers.js
+++ client/reducers.js
@@ -4,13 +4,11 @@
 import { combineReducers } from 'redux';
 
 // Import Reducers
-import app from './modules/App/AppReducer';
 import branches, { activeBranch } from './modules/App/components/reducers/BranchReducer';
 import projects from './modules/App/components/reducers/ProjectsReducer';
 
 // Combine all reducers into one root reducer
 export default combineReducers({
-  app,
   branches,
   activeBranch,
   projects,
