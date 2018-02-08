50a7271feaad188c7c754b6cd2277fc833c51c63
jmario mejia <jmariomejiap@gmail.com>
2018-02-07 19:22:05 -0800

    removing unused components, routes, adding empty projects page conditional render FIXING TEST

--- client/modules/App/tests.js
+++ client/modules/App/tests.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
-import Settings from './settingsPage';
+import EmptyProjectPage from './EmptyProject';
 
 test('Settings page renders properly', t => {
-  const wrapper = shallow(<Settings />);
+  const wrapper = shallow(<EmptyProjectPage />);
 
   t.is(wrapper.type(), 'div');
   t.is(wrapper.find('div').length, 1);
-  t.is(wrapper.find('h1').first().text(), 'SETTINGS PAGE!');
-  t.is(wrapper.find('h1').length, 1);
-  t.is(wrapper.find('RaisedButton').length, 1);
+  t.is(wrapper.find('h2').length, 1);
+  t.is(wrapper.find('h2').first().text(), 'No Projects have been created yet');
+  t.is(wrapper.find('h3').length, 1);
+  t.is(wrapper.find('h3').first().text(), 'Create your first project');
 });
 
